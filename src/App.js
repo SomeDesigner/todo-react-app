55937efda7c50e3d17f6a1f65bddbdb09a660540
Jon Stolte <jon.stolte@designdierepeat.com>
2022-02-02 17:28:23 -0500

    redoing file structure
    the old repo was a mess, going to fix the file structure. next step to create 'todo form' branch.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import {} from 'react';s
-import TodoList from './components/todoList/TodoList';
+import TodoList from './components/organisms/TodoList';
 
 function App() {
   return (
